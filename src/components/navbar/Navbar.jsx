007f74373841ea818c6b262669a79008d9943c14
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2023-12-19 18:31:09 +0530

    Dropdown Menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 // import React from 'react'
 import { BiPhoneCall, BiSolidSun } from 'react-icons/bi';
+import { FaCaretDown } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <header className=" bg-navbar text-white">
+    <header className=" bg-navbar text-white border-b-[1px] border-primary/50">
       <nav className="container flex items-center justify-between h-[70px] py-2">
         {/* Logo section */}
         <div className=" text-2xl md:text-3xl text-white">
@@ -13,8 +14,23 @@ const Navbar = () => {
         </div>
         {/* Desktop menu section */}
         <div>
-          <ul className=" flex gap-4 items-center">
-            <li>Home</li>
+          <ul className=" flex  items-center gap-10">
+            <li className=" group relative cursor-pointer">
+              <a href="#" className=" flex items-center gap-[2px] h-[72px]">
+                Home{' '}
+                <span>
+                  <FaCaretDown className=" transition-all duration-200 group-hover:rotate-180" />
+                </span>
+              </a>
+              {/* Dropdown section */}
+              <div className=" absolute -left-9 z-[99999] hidden w-[150px] bg-white shadow-md p-2 text-black rounded-md group-hover:block">
+                <ul className=" space-y-3">
+                  <li className=" p-2 hover:bg-violet-200"> Services</li>
+                  <li className=" p-2 hover:bg-violet-200">About Us</li>
+                  <li className=" p-2 hover:bg-violet-200">Privacy Policy</li>
+                </ul>
+              </div>
+            </li>
             <li>Services</li>
             <li>Contact Us</li>
             <li>
