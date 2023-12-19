4342d7d19ab23c7480576096cec8ea1211bd063b
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2023-12-19 14:17:21 +0530

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className=" bg-navbar text-white">
+      <nav className="container">
+        {/* Logo section */}
+        <div className=" text-2xl md:text-3xl text-white">
+          <a href="#">
+            COZ<span className=" inline-block font-bold text-primary">WEB</span>
+          </a>
+        </div>
+        {/* Desktop menu section */}
+        {/* Mobile menu section */}
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
