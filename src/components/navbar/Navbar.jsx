8e6fd8f71b3f83b3b77564486fc180b2749a50cf
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2023-12-19 14:46:18 +0530

    Responsive Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 // import React from 'react'
+import { BiPhoneCall, BiSolidSun } from 'react-icons/bi';
 
 const Navbar = () => {
   return (
     <header className=" bg-navbar text-white">
-      <nav className="container">
+      <nav className="container flex items-center justify-between h-[70px] py-2">
         {/* Logo section */}
         <div className=" text-2xl md:text-3xl text-white">
           <a href="#">
@@ -11,6 +12,28 @@ const Navbar = () => {
           </a>
         </div>
         {/* Desktop menu section */}
+        <div>
+          <ul className=" flex gap-4 items-center">
+            <li>Home</li>
+            <li>Services</li>
+            <li>Contact Us</li>
+            <li>
+              <div className=" flex items-center gap-4">
+                <div>
+                  <BiPhoneCall className=" text-2xl h-[40px] w-[40px] rounded-md p-2 text-white bg-primary hover:bg-primary/90" />
+                </div>
+                <div>
+                  <p>Call us on</p>
+                  <p>
+                    <a href="tel:+94712413634">+94 71-241-3634</a>
+                  </p>
+                </div>
+              </div>
+            </li>
+            {/* Light and dark mode switcher */}
+            <BiSolidSun className=" text-2xl" />
+          </ul>
+        </div>
         {/* Mobile menu section */}
       </nav>
     </header>
