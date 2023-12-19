08a6d58a732bf73eb17288ee244cddf532a3c299
Dilanka Liyanagama <prasadliyanagamad@gmail.com>
2023-12-19 21:42:15 +0530

    Navbar Dropdown using Tailwind CSS

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -31,7 +31,77 @@ const Navbar = () => {
                 </ul>
               </div>
             </li>
-            <li>Services</li>
+            <li className=" group cursor-pointer">
+              <a href="#" className=" flex items-center gap-[2px] h-[72px]">
+                Services{' '}
+                <span>
+                  <FaCaretDown className=" transition-all duration-200 group-hover:rotate-180" />
+                </span>
+              </a>
+              {/* Dropdown full width section */}
+              <div className=" absolute left-0 z-[99999] hidden w-full rounded-b-3xl bg-white p-2 text-black shadow-md group-hover:block">
+                <div className=" grid grid-cols-3 gap-5">
+                  <div className=" overflow-hidden">
+                    <img
+                      src="https://picsum.photos/200"
+                      alt="not found"
+                      className=" max-h-[300px] w-full rounded-b-3xl object-fill"
+                    />
+                  </div>
+                  <div className=" col-span-2">
+                    <h1 className=" text-xl font-semibold pb-3">
+                      Best Selling
+                    </h1>
+                    <p className=" text-sm text-slate-600">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Eius alias voluptates odit cumque quis aliquam nesciunt
+                      delectus saepe! Accusamus impedit rem officia provident
+                      fuga. Deleniti suscipit quos eveniet beatae accusamus?
+                      <div className=" grid grid-cols-3 mt-4">
+                        <div>
+                          <h1 className=" pb-1 text-xl font-semibold">
+                            Development
+                          </h1>
+                          <ul className=" space-y-2">
+                            <li className=" cursor-pointer text-black/80 hover:text-primary">
+                              Web Development
+                            </li>
+                            <li className=" cursor-pointer text-black/80 hover:text-primary">
+                              Mobile Development
+                            </li>
+                            <li className=" cursor-pointer text-black/80 hover:text-primary">
+                              Software Development
+                            </li>
+                          </ul>
+                        </div>
+                        <div>
+                          <h1 className=" pb-1 text-xl font-semibold">
+                            Development
+                          </h1>
+                          <ul className=" space-y-2">
+                            <li className=" cursor-pointer text-black/80 hover:text-primary">
+                              Web Development
+                            </li>
+                            <li className=" cursor-pointer text-black/80 hover:text-primary">
+                              Mobile Development
+                            </li>
+                            <li className=" cursor-pointer text-black/80 hover:text-primary">
+                              Software Development
+                            </li>
+                          </ul>
+                        </div>
+                        <div>
+                          <img
+                            src="https://picsum.photos/180"
+                            alt="not found"
+                          />
+                        </div>
+                      </div>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </li>
             <li>Contact Us</li>
             <li>
               <div className=" flex items-center gap-4">
